e5d4303adf8c57ba5e10ac9ac9b1375a7162b468
droidhuman <iv@l1t.ru>
2021-01-07 15:38:47 +0300

    modified App.java

--- java_tests/java_game/com/javagame/App.java
+++ java_tests/java_game/com/javagame/App.java
@@ -1,4 +1,12 @@
 package com.javagame;
 
-public class App {
+import javax.swing.*;
+
+public class App extends JFrame {
+    public App() {
+        initUI();
+    }
+    private void initUI() {
+
+    }
 }
